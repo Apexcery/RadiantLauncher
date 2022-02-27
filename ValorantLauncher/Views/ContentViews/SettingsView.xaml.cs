0a3c4cb6581660b9d74e8c0e8b3b88b546c85bd9
Stuart Gleeson <stuart.gleeson98@gmail.com>
2022-02-27 20:11:00 +0000

    Created help & about view with Q&A and links.

--- ValorantLauncher/Views/ContentViews/SettingsView.xaml.cs
+++ ValorantLauncher/Views/ContentViews/SettingsView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
 
 namespace ValorantLauncher.Views.ContentViews
 {
@@ -7,6 +10,38 @@ namespace ValorantLauncher.Views.ContentViews
         public SettingsView()
         {
             InitializeComponent();
+        }
+
+        private void RequestNavigateHandler(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = e.Uri.ToString(),
+                UseShellExecute = true
+            });
+        }
+
+        private void CreditsIconButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            var tag = button.Tag.ToString();
+
+            var psi = new ProcessStartInfo
+            {
+                UseShellExecute = true
+            };
+
+            switch (tag.ToLower())
+            {
+                case "twitter":
+                    psi.FileName = "Https://twitter.com/Apexcery";
+                    break;
+                case "github":
+                    psi.FileName = "Https://github.com/Apexcery";
+                    break;
+            }
+
+            Process.Start(psi);
         }
     }
 }
